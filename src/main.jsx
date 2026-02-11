981f57db618356e1b33a469df0dbc54613ef55d3
VaishnaviGund42 <vaishnavigund42@gmail.com>
2026-02-12 01:10:10 +0530

    react routing path added to main file

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { BrowserRouter,Routes,Route } from "react-router";
+import {createRoot} from "react-dom/client"
+import Home from './views/Home';
+import Vegitable from './views/Vegitable';
+import MyCart from './views/MyCart';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const root=createRoot(document.getElementById('root'));
+
+root.render(
+  <>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Vegitable" element={<Vegitable />} />
+        <Route path="/MyCart" element={<MyCart />} />
+      </Routes>
+    </BrowserRouter>
+  </>
+
 )
